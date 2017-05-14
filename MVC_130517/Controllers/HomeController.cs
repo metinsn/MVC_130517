8b87fff0e3618ce23d2932322478dca1266ed88d
Metin <metinsn@outlook.com>
2017-05-14 20:40:29 +0300

    Yavuz Hocanın Örneği 14052017

---
+++ MVC_130517/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_130517.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            if(Session["name"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login", "Home");
+            }
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(FormCollection frm)
+        {
+            string uname = frm.Get("userName");
+            string pass = frm.Get("password");
+            
+            if (uname == "yavuz" && pass == "1234")
+            {
+                Session["name"] = "yavuz";
+                Session["pass"] = "1234";
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Mesaj = "Hatalı Bilgiler!";
+                return View();
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+            return RedirectToAction("Login", "Home");
+        }
+    }
+}
---
+++ MVC_130517/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_130517.MvcApplication" Language="C#" %>
